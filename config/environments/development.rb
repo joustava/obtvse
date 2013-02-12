69a4196e130a02fc3d5540b41ba1b56f9f60b0d5
joustava <joustava@gmail.com>
2013-02-12 13:08:23 +0100

    Fix asset settings and icon paths

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Obtvse::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
+  config.serve_static_assets = false
   config.assets.compress = false
 
   # Expands the lines which load the assets
